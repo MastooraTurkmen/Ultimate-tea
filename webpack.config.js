48eda636890795ce5eb10107a52e7fe49a21d41f
Mastoora Turkmen <mastooraturkmen@gmail.com>
2023-10-11 20:11:43 +0430

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,21 +5,5 @@ module.exports = {
   "entry": {
     "index": "./index"
   },
-  "module": {
-    "rules": [
-      {
-        "use": {
-          "loader": "babel-loader",
-          "options": {
-            "presets": [
-              "babel-preset-env",
-              "babel-preset-react"
-            ]
-          }
-        },
-        "exclude": /node_modules/,
-        "test": /\.js$/
-      }
-    ]
-  }
+  
 };

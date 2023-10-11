e904781d79692ef18ac7d89b4cfa82825b1196cc
MastooraTurkmen <janmastoorajan@gmail.com>
2023-10-11 08:12:11 +0430

    set up

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const get = element => document.getElementById(element);
 
 let open = get("menu-btn");
 let nav = get("nav");
-const exit = get("exit-btn");
+let exit = get("exit-btn");
 
 open.addEventListener('click', () => {
     nav.classList.add('open-nav');
